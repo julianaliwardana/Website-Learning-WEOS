eff9d38c1fba2ec39d7256e5aa26bb352475d082
Julian_A_Wardana <julian.wardana@binus.ac.id>
2021-08-28 15:30:17 +0700

    edit homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container" style="margin-top:100px;">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -30,5 +30,67 @@
             </div>
         </div>
     </div>
+</div> --}}
+<div class="container">
+    <div id="carouselExampleDark" class="carousel carousel-dark slide " data-bs-ride="carousel">
+        <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+            <img src="{{ asset("image/0.png") }}" class="d-block w-50 gambar" alt="#">
+            <div class="carousel-caption d-none d-md-block" style="color:black; font-weight: bold;" >
+            <h5 >C Programming</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+            <img src="{{ asset("image/1.png") }}" class="d-block w-50 gambar" alt="#">
+            <div class="carousel-caption d-none d-md-block" style="color:black; font-weight: bold;">
+            <h5>C++ Programming</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+            </div>
+        </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+<div class="footer-dark">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>Courses</h3>
+                    <ul>
+                        <li><a href="#">C Programming</a></li>
+                        <li><a href="#">Civil Engineering</a></li>
+                        <li><a href="#">Catering Courses</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Company</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 item text">
+                    <h3>WEOS</h3>
+                    <p>WEOS is an online learning web that helps students who have difficulty learning their course material due to the difficulty of independent and unstructured learning. </p>
+                </div>
+                <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+            </div>
+            <p class="copyright">WEOS © 2018</p>
+        </div>
+    </footer>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     {{-- Bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
@@ -25,10 +26,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{asset("image\logo.png")}}" style="width:52px" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -37,7 +38,19 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li clas="nav-item my" style="height: 30px;">
+                            <a href="{{ route('show-roadmap') }}" style="padding: 8px">Roadmap</a>
+                        </li>
+                        <li class="nav-item dropdown ml-2">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Course
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">C</a>
+                                <a class="dropdown-item" href="#">C++</a>
+                                <a class="dropdown-item" href="#">Java</a>
+                            </div>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -76,9 +89,35 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
+        <div class="customNavbar w-100">
+            <div class="row mx-0 my-custom-nav justify-content-start align-items-center">
+                <div class="ml-5 col-lg-1 col-md-2">
+                    <a href="#">
+                        <img src="{{ asset('image/logo.png') }}" width="80px">
+                    </a>
+                </div>
+                <div class="col-lg-1 col-md-2 col-sm-6">
+                    <div class="btn-group">
+                        <a href="#" id="courses">Roadmap</a>
+                    </div>
+                </div>
+                <div class="col-lg-1 col-md-2 col-sm-6">
+                    <a href="#" id="courses">Courses</a>
+                </div>
+                <div class="col-lg-1 col-md-2 col-sm-6">
+                    <a href="#" id="forms">Post</a>
+                </div>
+                <div class="col-lg-1 offset-lg-5 col-md-1 offset-md-1 col-sm-6 btn btn-success">
+                    <a href="#" style="text-decoration: none; color: white;">Login</a>
+                </div>
+                <div class="col-lg-1 ml-1 col-md-1 col-sm-6 btn btn-success">
+                    <a href="#" style="text-decoration: none; color: white;">Register</a>
+                </div>
+            </div>
+        </div>
 
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
     </div>
