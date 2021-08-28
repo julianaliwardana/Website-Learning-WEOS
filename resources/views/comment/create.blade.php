1ca44fae054331a2d4326b53656a646359a840e0
Julian_A_Wardana <julian.wardana@binus.ac.id>
2021-08-28 18:58:26 +0700

    edit comment forum v2

--- resources/views/comment/create.blade.php
+++ resources/views/comment/create.blade.php
@@ -6,10 +6,6 @@
             <div class="row mb-3">
                 <div class="col-xl-11 col-md-10 col-sm-10 col-10 p-1">
                     <textarea class="form-control w-100" id="commentInput" name="content" placeholder="Comment"></textarea>
-                    {{-- @error('content') is-invalid @enderror --}}
-                    {{-- @error('content')
-                        <div class="invalid-feedback bg-danger text-white rounded mt-2 p-2">{{$message}}</div>
-                    @enderror --}}
                 </div>
                 <div class="col-xl-1 col-md-2 col-sm-2 col-2 p-1">
                     <button type="submit" class="btn btn-primary w-120 h-100" id="">Submit</button>
